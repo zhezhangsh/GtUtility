39442179820a00244d14cf509845f9216826e419
zhangz <zhangz@email.chop.edu>
2015-03-27 11:12:10 -0400

    folder for example runs

--- R/analyzeTrio.r
+++ R/analyzeTrio.r
@@ -7,6 +7,8 @@ analyzeTrio<-function(geno, trio, analyses=list(
   # trio        A data.frame of three ID columns: child, parent1, parent2; corresponding to column names in the <geno> matrix
   # analyses    Whether to run a specific analysis, such as summarize de novo mutations
   
+  geno[is.na(geno)]<--1;
+  
   # all results
   results<-list();
   
@@ -21,11 +23,16 @@ analyzeTrio<-function(geno, trio, analyses=list(
     
     ##################################################################################################
     # summarize de novo mutations
-    if (exists('analyses$de.novo')) if (analyses$de.novo[1] == TRUE) {
+    if (analyses$de.novo) {
       ttl<-!is.na(g[[1]]) & !is.na(g[[2]]) & !is.na(g[[3]]) & g[[2]]==0 & g[[3]]==0;
       yes<-ttl & g[[1]]==1;
       no<-ttl & g[[1]]==0;
       results$de.novo<-list(Yes=yes, No=no, Summary=list(By.Variant=cbind(Yes=rowSums(yes), No=rowSums(no)), By.Sample=cbind(Yes=colSums(yes), No=colSums(no))));
+    } else if (analyses$ar) {
+      ct<-lapply(0:2, function(i) g[[1]]==i & g[[2]]==1 & g[[3]]==1);
+      names(ct)<-c('AA', 'AB', 'BB');
+      smm<-list(Total=sapply(ct, sum), By.Variant=sapply(ct, rowSums), By.Sample=sapply(ct, colSums));
+      results$auto.recessive<-list(Yes=ct, Summary=smm);
     }
     
     results;
